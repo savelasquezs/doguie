833916abe3d3bc9c808b262c20aadb01828d3b95
santyvano <santyvano@outlook.com>
2022-11-03 17:00:02 -0500

    lo que paso paso

---
+++ src/main.js
@@ -0,0 +1,15 @@
+const baseUrl="https://api.thedogapi.com/v1/images/search"
+const apiKey="live_gVbQrZiQSrWD3NynLQiQxMExPvoS5TxiJ8Cvu5yK2kU5W1jxFJ60T1XQfCupsMSI"
+const image=document.getElementById('imgGatito')
+const button=document.querySelector('button')
+
+const consultar=async()=>{
+    const response=await fetch(baseUrl)
+    const responseJson=await response.json()
+    const data=responseJson[0]
+    console.log(data.url)
+    image.src=data.url
+
+}
+
+button.addEventListener('click',consultar)
